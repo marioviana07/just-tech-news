76722bfd0a19e73e8c00fd5174f11196c75fd257
marioviana07 <mario_viana_2005@yahoo.com>
2020-10-30 13:41:51 -0700

    comments

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,21 +3,6 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 
-// router.get('/', (req, res) => {
-//     res.render('homepage', {
-//         id: 1,
-//         post_url: 'https://handlebarsjs.com/guide/',
-//         title: 'Handlebars Docs',
-//         created_at: new Date(),
-//         vote_count: 10,
-//         comments: [{}, {}],
-//         user: {
-//             username: 'test_user'
-//         }
-//     });
-// });
-
-
 router.get('/', (req, res) => {
     Post.findAll({
             attributes: [
@@ -41,6 +26,7 @@ router.get('/', (req, res) => {
             ]
         })
         .then(dbPostData => {
+            console.log(dbPostData[0]);
             // pass a single post object into the homepage template
             res.render('homepage', dbPostData[0]);
         })
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require("./Post");
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -8,6 +9,8 @@ User.hasMany(Post, {
 
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-})
+});
+
+
 
 module.exports = { User, Post };
